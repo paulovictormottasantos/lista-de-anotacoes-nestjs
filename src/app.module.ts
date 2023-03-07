f90c3d39ef9fe45add3d18fc1758b0b85304911c
Paulo Santos <126355637+paulovictormottasantos@users.noreply.github.com>
2023-03-07 13:25:56 -0300

    feat(notes): generate resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AccountsModule } from './accounts/accounts.module';
+import { NotesModule } from './notes/notes.module';
 
 @Module({
-  imports: [PrismaModule, AccountsModule]
+  imports: [PrismaModule, AccountsModule, NotesModule]
 })
 export class AppModule {}
