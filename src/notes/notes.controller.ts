820bdf88a9331e3f3a1e7ab085a40d86b47421e8
Paulo Santos <126355637+paulovictormottasantos@users.noreply.github.com>
2023-03-07 14:21:30 -0300

    feat(notes): implement create note route

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { CreateNoteDto } from './dtos/create-note.dto';
 import { NotesService } from './notes.service';
 
-@Controller('notes')
+@ApiTags('notes')
+@Controller('note')
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
+
+  @Post('/create/:accountId')
+  @ApiBody({
+    type: CreateNoteDto,
+  })
+  async createNote(
+    @Param('accountId') accountId: string,
+    @Body() note: CreateNoteDto,
+  ) {
+    return await this.notesService.createNote(accountId, note);
+  }
 }
--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateNoteDto } from './dtos/create-note.dto';
 
 @Injectable()
-export class NotesService {}
+export class NotesService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createNote(
+    accountId: string,
+    note: CreateNoteDto,
+  ): Promise<HttpException> {
+    const foundAccount = await this.prismaService.account.findUnique({
+      where: {
+        id: accountId,
+      },
+    });
+
+    if (!foundAccount) {
+      throw new HttpException('Account not found.', HttpStatus.NOT_FOUND);
+    }
+
+    const createdNote = await this.prismaService.note.create({
+      data: {
+        title: note.title,
+        text: note.text,
+        accountId,
+      },
+    });
+
+    if (!createdNote) {
+      throw new HttpException(
+        'Note not created.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+
+    throw new HttpException('Note successfully created.', HttpStatus.CREATED);
+  }
+}
